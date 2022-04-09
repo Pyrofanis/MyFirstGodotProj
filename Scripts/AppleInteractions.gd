0b279efdac7c0268cbee1f5079e5fdf2791a91ea
Athanasios Nektarios Karachalios Stagkas <nasos.karachalios.stagas@gmail.com>
2022-04-09 15:38:00 +0300

    Need ToFix applePositioning

--- Scripts/AppleInteractions.gd
+++ Scripts/AppleInteractions.gd
@@ -1,12 +1,15 @@
 extends Area2D
 
-export var places=Vector2.ZERO
+var coli_name
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-	
+func _process(delta):
+	if (coli_name!=null && coli_name.to_lower()=="playerArea2d"):
+		print("test")
+	pass
 
-
-func _on_Area2D_area_entered(area:Area2D):
-	get_parent().coli_name=area.get_parent().get_parent().name
+func _on_Area2D_body_entered(body):
+	print("piani")
+	# coli_name=body.get_node_name()
 	pass # Replace with function body.
--- Scripts/AppleParrent.gd
+++ Scripts/AppleParrent.gd
@@ -1,32 +1,18 @@
-extends KinematicBody2D
+extends Node
 
-export var coli_name=""
-export var places=[Vector3.ZERO,Vector3.ZERO]
-export var score=0
 
-var new_pos=Vector3.ZERO
-func _process(delta):
-	if (coli_name!=null&&coli_name.to_lower()=="fox"):
-		when_to_act()
-		add_score_and_reset()
-	pass
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
 
-func _calculate_new_pos(locs:Array):
-	var x =rand_range(locs[0].x,locs[1].x)
-	var y=rand_range(locs[0].x,locs[1].y)
-	new_pos=Vector2(x,y)
-	pass
-func when_to_act():
-	_calculate_new_pos(places)
-	change_position(new_pos)
-	pass
-func add_score_and_reset():
-	score+=1
-	coli_name=null
-	print(coli_name,score)
-	pass
 
-func change_position(loc:Vector2):
-	position=loc
-	print($Area2D.position)
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+func _changePosition():
 	pass
